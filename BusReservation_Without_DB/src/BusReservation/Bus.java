b11294124cd93fadfc9180b201dcc9085324f35b
Vigneshwaran-PS <psviki29@gmail.com>
2024-01-01 15:53:24 +0530

    Bus class is updated

--- BusReservation_Without_DB/src/BusReservation/Bus.java
+++ BusReservation_Without_DB/src/BusReservation/Bus.java
@@ -1,4 +1,42 @@
 package BusReservation;
 
 public class Bus {
+
+    private int busNo;
+    private boolean ac;
+    private int capacity;
+
+    Bus(int busNo , boolean ac , int capacity){
+        this.capacity = capacity;
+        this.busNo = busNo;
+        this.ac = ac;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getBusNo() {
+        return busNo;
+    }
+
+    public boolean isAc() {
+        return ac;
+    }
+
+    public void setBusNo(int busNo) {
+        this.busNo = busNo;
+    }
+
+    public void setAc(boolean ac) {
+        this.ac = ac;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void displayBusInf0(){
+        System.out.println("\t Bus No : " + getBusNo() + "\t Ac : " + isAc() + "\t Capacity : " + getCapacity());
+    }
 }
